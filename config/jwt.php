abb662f354ba874352eeeb59533e6a34b291d693
Dejan Babić <babicaja@gmail.com>
2019-07-11 10:37:41 +0200

    Edit config file to please OCD

--- config/jwt.php
+++ config/jwt.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'algorithm' => env('JWT_ALG', 'sha256'), //hash_hmac_algos()
+    'algorithm' => env('JWT_ALG', 'sha256'), // hash_hmac_algos()
     'expires' => env('JWT_EXP', 15), // minutes
     'secret' => env('JWT_KEY', 'secret'),
     'checks' => [
-       \JWT4L\Checks\Structure::class,
-       \JWT4L\Checks\Signature::class,
-       \JWT4L\Checks\Expired::class
+        \JWT4L\Checks\Structure::class,
+        \JWT4L\Checks\Signature::class,
+        \JWT4L\Checks\Expired::class
     ]
 ];
